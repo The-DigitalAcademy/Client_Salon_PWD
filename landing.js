44c7951098b0b83c53b617dd3eb0c2542a0b7186
mthimkulu23 <thabang23mthimkulu@gmail.com>
2024-03-05 14:36:24 +0200

    salons all putted

---
+++ landing.js
@@ -0,0 +1,6 @@
+const menuIcon = document.getElementById('menu-icon');
+
+menuIcon.addEventListener('click', function() {
+  menuIcon.classList.toggle('active');
+  // Add your additional code here for the desired functionality
+});
