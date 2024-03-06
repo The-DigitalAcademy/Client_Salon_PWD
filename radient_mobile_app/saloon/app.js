8ef44984ea1123e701fa7145bfdc6a9c97ef852c
mthimkulu23 <146940556+mthimkulu23@users.noreply.github.com>
2024-03-06 12:25:36 +0200

    Add files via upload

---
+++ radient_mobile_app/saloon/app.js
@@ -0,0 +1,25 @@
+
+
+
+const dropdownMenu = document.getElementById('dropdownMenu')
+let showDropdown = false
+const options = document.getElementsByClassName('option')
+
+
+
+
+function toggleMenu() {
+   if (showDropdown) {
+    dropdownMenu.classList.remove('dropdown')
+    dropdownMenu.classList.add('dropdownClose')
+    showDropdown = false
+   } else {
+    dropdownMenu.classList.remove('dropdownClose')
+    dropdownMenu.classList.add('dropdown')
+    showDropdown = true
+   }
+}
+
+for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener('click', () => toggleMenu())
+}
