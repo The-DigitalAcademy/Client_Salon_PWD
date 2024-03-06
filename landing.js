97ceceeb1f35c54897a787c96075d779280be368
mthimkulu23 <146940556+mthimkulu23@users.noreply.github.com>
2024-03-06 12:19:09 +0200

    Delete landing.js

--- landing.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.getElementById('menu-icon');
-
-menuIcon.addEventListener('click', function() {
-  menuIcon.classList.toggle('active');
-  // Add your additional code here for the desired functionality
-});
