7276f9bb6e1ac8774c4958f546d7c81a47f01c27
mthimkulu23 <146940556+mthimkulu23@users.noreply.github.com>
2024-03-15 08:08:32 +0200

    Delete radient_mobile_app directory

--- radient_mobile_app/Client_Salon_PWD/landing.js
+++
@@ -1,4 +0,0 @@
-function toggleDropdown() {
-    var dropdownMenu = document.getElementById('dropdown-menu');
-    dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
-  }
