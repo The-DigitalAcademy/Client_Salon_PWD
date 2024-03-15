0686b1d9977bb841672f58f43f1ba6df38bbc8f8
mthimkulu23 <thabang23mthimkulu@gmail.com>
2024-03-15 15:29:47 +0200

    pages are linked on

--- Flask_App_Salon 12/Hair.py
+++ Flask_App_Salon 12/Hair.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
 
 
-hair_blueprint = Blueprint('hair', __name__)
+hair_blueprint = Blueprint('hair_salon', __name__)
 
 
 @hair_blueprint.route('/hair_salon', methods=['GET', 'POST'])
