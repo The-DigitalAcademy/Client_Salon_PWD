b261b553ce4a5d8764f852287a4d32a5939227a4
mthimkulu23 <thabang23mthimkulu@gmail.com>
2024-03-18 11:16:11 +0200

    finished with nails page going to another page on category

--- Flask_App_Salon 12/nails.py
+++ Flask_App_Salon 12/nails.py
@@ -5,5 +5,5 @@ nails_blueprint = Blueprint('nails', __name__)
 
 
 @nails_blueprint.route('/nails', methods=['GET', 'POST'])
-def hair_salon():
+def nails():
     return render_template('nails.html')
