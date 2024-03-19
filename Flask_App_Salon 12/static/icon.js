6c4f56d8211edb3218f7483ebc75fa0ccbdf2da9
mthimkulu23 <thabang23mthimkulu@gmail.com>
2024-03-19 08:32:34 +0200

    updated icon onclick to go back functionality on nails category

---
+++ Flask_App_Salon 12/static/icon.js
@@ -0,0 +1,10 @@
+document.getElementById("icon").onclick = function() {
+    // Add your code here to handle the onclick event
+    goToLandingPage(hair_Salon);
+  };
+  
+  function goToLandingPage(hair_Salon) {
+    // Add your code here to navigate back to the landing page
+    // For example:
+    window.location.href = 'http://127.0.0.1:5000/hair_salon'; // Replace 'landing.html' with the actual URL of your landing page
+  }
