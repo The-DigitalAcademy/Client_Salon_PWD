dd4da3fe05cb7d683200e8362d86826d1a7a62a5
mthimkulu23 <thabang23mthimkulu@gmail.com>
2024-03-25 15:48:22 +0200

    register on py has been fixed

--- Flask_App_Salon 12/static/icon.js
+++ Flask_App_Salon 12/static/icon.js
@@ -7,4 +7,3 @@ function goToServiceSerene(Service_glit) {
   window.location.href = 'http://127.0.0.1:5000/Service_glit';
 }
 
-
---
+++ register.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+# from DB import mongo
+from flask_pymongo import PyMongo
+
+mongo = PyMongo()
+register_blueprint = Blueprint('register', __name__)
+
+@register_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        # Check if username already exists
+        existing_user = mongo.db.users.find_one({'email': email})
+        if existing_user:
+            flash('Username already exists. Please choose another one.', 'error')
+            return redirect(url_for('register.register'))
+
+        # Check if passwords match
+        if password != confirm_password:
+            flash('Passwords do not match. Please try again.', 'error')
+            return redirect(url_for('register.register'))
+
+        # Insert new user into the database
+        new_user = {
+            'name': name,
+            'email': email,
+            'password': password  # You should hash the password for security
+        }
+        mongo.db.users.insert_one(new_user)
+        
+        flash('Registration successful! You can now log in.', 'success')
+        return redirect(url_for('login.login'))
+
+    return render_template('register.html')
