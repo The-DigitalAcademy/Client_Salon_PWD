0b1cc690415f948b81c251ce44972f611a153bd1
mthimkulu23 <thabang23mthimkulu@gmail.com>
2024-03-26 14:12:53 +0200

    lumina img for choose service been fixed

---
+++ Flask_App_Salon 12/static/Choose_lumina.js
@@ -0,0 +1,13 @@
+
+
+  
+  function goToLandingPage1() {
+    // code here to navigate back to the landing page
+    window.location.href = 'http://127.0.0.1:5000/Choose_lumina'; // Replace 'landing.html' with the actual URL of your landing page
+  }
+
+  function goToLandingPage2( ) {
+ // Replace the URL "/landing-page" with the actual URL or route of the landing page
+    window.location.href = "http://127.0.0.1:5000/Lumina_salon";
+          }
+   
